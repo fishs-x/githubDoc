516e5c05e8d06a3f21f45651c7f4c48eeba0a46d
eee <zhangzhch2016@gmail.com>
2019-04-08 18:13:37 +0800

    add field

--- models/docs.py
+++ models/docs.py
@@ -6,7 +6,8 @@ class Docs(BaseModel):
     title = db.Column('title', db.String(120))
     desc = db.Column('desc', db.String(255))
     url = db.Column('url', db.String(255))
+    author = db.Column('author', db.String(255))
 
     @property
     def to_json(self):
-        return {'title': self.title, 'desc': self.desc, 'url': self.url, 'id': self.id}
+        return {'title': self.title, 'desc': self.desc, 'url': self.url, 'id': self.id, 'author': self.author}
