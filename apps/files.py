1b095055cf58299319a79a07fa81c0dc01858209
eee <zhangzhch2016@gmail.com>
2019-04-09 11:01:31 +0800

    add 文件展示API

--- apps/files.py
+++ apps/files.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 from flask import Blueprint
 from apps import response
 
@@ -8,5 +9,5 @@ bp = Blueprint('feedback', __name__, url_prefix='/api/v1')
 @bp.route('/<path>/files', methods=['GET'])
 def get_files(path):
     root_dir = '/home/www/test'
-    files = os.listdir(root_dir + path)
+    files = os.listdir(root_dir + urllib.parse.unquote(path, encoding='utf-8', errors='replace'))
     return response(files)
